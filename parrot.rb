febfe68772c763aa6430c0d093318eb30ee5acf9
Damini Varu <damvaru4@gmail.com>
2019-09-26 13:21:24 +0000

    Automatically backed up by Learn

--- parrot.rb
+++ parrot.rb
@@ -1,20 +1,9 @@
 
 
 def parrot(number)
-  number + 2
+  puts 
 end
 
-def add_two(number)
-  number + 2
-end
-
-puts add_two(3)
-
-def add_two(number)
-  number + 2
-end
-
-p add_two(3)
 
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
